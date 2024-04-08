1635d42938044a4d6b3adc76f86df9158048f8e7
Rohitt <rohitsarje17@gmail.com>
2024-04-08 08:50:29 +0530

    Used Joblib to load and dump

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
-#import pickle 
+ 
 
 df = pd.read_csv('Datasets/IPLPlayersData.csv')
 
